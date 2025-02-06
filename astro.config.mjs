cb742af0d74b433a23adba2e312a1b86ff5a097e
atip.n@10xgroupholding.com <atip.n@10xgroupholding.com>
2025-02-06 15:16:14 +0700

    update

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+const isProduction = process.env.node == "prod";
+export default defineConfig({
+  site: "https://astro-moon-landing.netlify.app/",
+  integrations: [tailwind(), icon()],
+  prefetch: true,
+  site: "https://atpznn.github.io",
+  base: isProduction ? "/p" : "/",
+});
